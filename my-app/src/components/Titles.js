231f0e04e12a0d27b5444e939cba5906a664504f
Kasiban <kasibanoy@gmal.com>
2018-10-27 14:10:08 -0700

    weather checker created with react

---
+++ my-app/src/components/Titles.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Titles = () => (
+    <div>
+        <h1 className="title-container__title">Weather Checker</h1>
+        <p className="title-container__subtitle">Check the weather in your area and more...</p>
+    </div>
+);
+
+
+
+export default Titles;
