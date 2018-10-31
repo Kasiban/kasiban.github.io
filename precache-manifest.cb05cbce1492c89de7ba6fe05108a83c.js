0763fccb8a9534123d45c8d54809d8ae35d59216
Kasiban <kasibanoy@gmal.com>
2018-10-31 14:09:33 -0700

    Updates

---
+++ precache-manifest.cb05cbce1492c89de7ba6fe05108a83c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f40e6b1cf3222f32bdb3",
+    "url": "/my-app/static/css/main.7c6185ec.chunk.css"
+  },
+  {
+    "revision": "c1ddad756200c4014b6e6b765f91f91f",
+    "url": "/my-app/static/media/weather_app.c1ddad75.jpg"
+  },
+  {
+    "revision": "4a4f8cdd751f82755f82",
+    "url": "/my-app/static/js/1.4a4f8cdd.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "f40e6b1cf3222f32bdb3",
+    "url": "/my-app/static/js/main.f40e6b1c.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/my-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "4a4f8cdd751f82755f82",
+    "url": "/my-app/static/css/1.155ee53a.chunk.css"
+  },
+  {
+    "revision": "cf4b44db38da72f8c73a4983ef0df276",
+    "url": "/my-app/index.html"
+  }
+];
